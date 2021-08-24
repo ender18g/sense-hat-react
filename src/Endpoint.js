a97dc7920e36330196993b329cb93ae580bdec35
Allan Elsberry <allan.elsberry@gmail.com>
2021-08-24 11:25:37 -0400

    added send text

--- src/Endpoint.js
+++ src/Endpoint.js
@@ -1,7 +1,7 @@
 //const ENDPOINT = `http://192.168.1.249:5000`;
 
-const ENDPOINT = `http://10.3.141.1:5000`;
+//const ENDPOINT = `http://10.3.141.1:5000`;
 
-//const ENDPOINT = `http://${window.location.hostname}:5000`;
+const ENDPOINT = `http://${window.location.hostname}:5000`;
 
 export default ENDPOINT;
